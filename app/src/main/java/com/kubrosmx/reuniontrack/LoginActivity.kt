784420d08fa8ab08dcaf6ee5bc9257e445e8cd77
jesus <jose1824@live.com.mx>
2017-06-27 22:43:27 -0500

    Kotlin y Facebook API agregadas

---
+++ app/src/main/java/com/kubrosmx/reuniontrack/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.kubrosmx.reuniontrack
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.facebook.FacebookSdk
+import com.facebook.appevents.AppEventsLogger
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
--- app/src/main/java/com/kubrosmx/reuniontrack/SplashScreen.java
+++ app/src/main/java/com/kubrosmx/reuniontrack/SplashScreen.java
@@ -1,5 +1,7 @@
 package com.kubrosmx.reuniontrack;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,23 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        getSupportActionBar().hide();
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                redireccionar();
+            }
+        }, 5000);   //5 segundos
+
+    }
+
+    /**
+     *  Como no se puede redireccionar directo en el run del handler, se utiliza este metodo para
+     *  hacerlo
+     */
+    private void redireccionar(){
+        startActivity(new Intent(this, LoginActivity.class));
     }
 }
