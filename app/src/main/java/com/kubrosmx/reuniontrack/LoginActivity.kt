45fc24eb8b9993893545c76edbeb4ff646cdf5c6
jesus <jose1824@live.com.mx>
2017-07-02 22:39:58 -0500

    Agregado Login con Facebook funcional por medio de boton y automatico

--- app/src/main/java/com/kubrosmx/reuniontrack/LoginActivity.kt
+++ app/src/main/java/com/kubrosmx/reuniontrack/LoginActivity.kt
@@ -1,14 +1,84 @@
 package com.kubrosmx.reuniontrack
 
+import android.content.Context
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.facebook.FacebookSdk
-import com.facebook.appevents.AppEventsLogger
+import android.widget.Toast
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+import com.facebook.login.widget.LoginButton
+import java.util.*
 
 class LoginActivity : AppCompatActivity() {
+
+    companion object {
+        lateinit var instance: LoginActivity
+            private set
+    }
+
+    var callbackManager : CallbackManager = CallbackManager.Factory.create()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        instance = this
+
+        val loginFacebook = LoginButton(this)
+        loginFacebook.setReadPermissions("email")
+
+        validarLogin();
+
+        loginFacebook.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+            override fun onCancel() {
+                instance.toast("Login Cancelado")
+            }
+
+            override fun onSuccess(result: LoginResult?) {
+                instance.toast("Login Exitoso")
+                escribirPreferencia("face_logueado", "1")
+                var intent = Intent(instance, PrincipalActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+            override fun onError(error: FacebookException?) {
+                instance.toast("Error en el login "  + error?.message)
+            }
+        })
+
+    }
+
+    fun toast(message: CharSequence) =
+            Toast.makeText(instance, message, Toast.LENGTH_SHORT).show()
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+    }
+
+    fun escribirPreferencia(clave: String, valor: String) {
+        var pref = getSharedPreferences("pref1", Context.MODE_PRIVATE)
+        var editor = pref.edit();
+        editor.putString(clave, valor);
+        editor.commit()
+    }
+
+    fun obtenerPreferencia(clave: String): String? {
+        return getSharedPreferences("pref1", Context.MODE_PRIVATE).getString(clave, "0")
+    }
+
+    fun validarLogin(){
+        if (obtenerPreferencia("face_logueado").equals("1")) {
+            LoginManager.getInstance().logInWithReadPermissions(
+                    this,
+                    Arrays.asList("public_profile")
+            );
+        }
     }
 }
+
--- app/src/main/java/com/kubrosmx/reuniontrack/SplashScreen.java
+++ app/src/main/java/com/kubrosmx/reuniontrack/SplashScreen.java
@@ -1,9 +1,17 @@
 package com.kubrosmx.reuniontrack;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -14,12 +22,16 @@ public class SplashScreen extends AppCompatActivity {
 
         getSupportActionBar().hide();
 
+        obtenerHash();
+
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             public void run() {
                 redireccionar();
             }
         }, 5000);   //5 segundos
+
+
 
     }
 
@@ -29,5 +41,32 @@ public class SplashScreen extends AppCompatActivity {
      */
     private void redireccionar(){
         startActivity(new Intent(this, LoginActivity.class));
+    }
+
+    /**
+     *  Metodo para obtener el HASH del dispositivo
+     */
+    private void obtenerHash() {
+        PackageInfo info;
+        try {
+            info = getPackageManager().getPackageInfo(
+                    "com.kubrosmx.reuniontrack",
+                    PackageManager.GET_SIGNATURES
+            );
+            for (Signature signature : info.signatures) {
+                MessageDigest md;
+                md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String something = new String(Base64.encode(md.digest(), 0));
+                //String something = new String(Base64.encodeBytes(md.digest()));
+                Log.e("hash key", something);
+            }
+        } catch (PackageManager.NameNotFoundException e1) {
+            Log.e("name not found", e1.toString());
+        } catch (NoSuchAlgorithmException e) {
+            Log.e("no such an algorithm", e.toString());
+        } catch (Exception e) {
+            Log.e("exception", e.toString());
+        }
     }
 }
